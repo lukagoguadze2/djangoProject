4b569ee8f3ac191cec0d798a39080df9971d257d
Luka Goguadze <lukagoguadze2@gmail.com>
2024-10-04 17:22:16 +0400

    update store app

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('item/<int:_id>', views.item, name='item'),
+    path('item/<int:_id>/<str:component>', views.display_component, name='component')
 ]
