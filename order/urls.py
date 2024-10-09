91ae63352a192821a1a825594ef0d388b86784dc
Luka Goguadze <lukagoguadze2@gmail.com>
2024-10-09 21:21:59 +0400

    remove order views and urls

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('', views.order_list, name='order_list'),
-    path('create-order/', views.create_order, name='create_order'),
 ]
